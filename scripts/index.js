5ae89fed9e2cd7cbfc0b1796cfc977a4d44f4933
Robin <robin.django@gmail.com>
2022-12-09 12:40:16 +0100

    Added more comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,21 @@
-var menuOpen;
-const searchBtn = document.getElementById("search-btn-icon");
-
-const onPageLoad = () => {
-  menuOpen = false;
-};
+// Boolean variable to check if menu is open.
+// Assigned globally so it doesn't reassign on every click.
+var menuOpen = false;
 
 const menuBtnHandler = () => {
+  // Get all the elements we need to toggle/control
   const animation = document.getElementById("menu-btn-icon");
   const menuBubbleTop = document.getElementById("menu-bubble-top");
   const menuBubbleLower = document.getElementById("menu-bubble-lower");
   const pageTitle = document.getElementById("page-title");
   const navItems = Array.from(document.getElementsByClassName("nav-item"));
-  console.log(navItems);
 
+  // Toggle all the active classes to start the animations
   pageTitle.classList.toggle("page-title-active")
   menuBubbleTop.classList.toggle("menu-active-top")
   menuBubbleLower.classList.toggle("menu-active-lower")
 
+  // Check if menu is opening or closing, and play the correct Lottie animation
   if (menuOpen === true) {
     menuOpen = false;
     animation.getLottie().playSegments([14, 0], true);
@@ -25,6 +24,7 @@ const menuBtnHandler = () => {
     animation.getLottie().playSegments([0, 14], true);
   }
 
+  // Toggle the active class on the nav items
   for (let i = 0; i < navItems.length; i++) {
     navItems[i].classList.toggle("nav-item-active");
   }
