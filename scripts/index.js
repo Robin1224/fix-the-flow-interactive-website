ca00c6476d33aad0fa6a1fe1271feb80071bc6fb
Robin <robin.django@gmail.com>
2022-12-09 12:29:22 +0100

    Finished first iteration navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,21 @@
 var menuOpen;
+const searchBtn = document.getElementById("search-btn-icon");
 
 const onPageLoad = () => {
   menuOpen = false;
-
-  const searchBtn = document.querySelector(".search-btn-icon");
-  searchBtn.addEventListener("mouseover", searchBtnHandler);
 };
 
 const menuBtnHandler = () => {
-  const animation = document.querySelector(".menu-btn-icon");
+  const animation = document.getElementById("menu-btn-icon");
   const menuBubbleTop = document.getElementById("menu-bubble-top");
   const menuBubbleLower = document.getElementById("menu-bubble-lower");
   const pageTitle = document.getElementById("page-title");
+  const navItems = Array.from(document.getElementsByClassName("nav-item"));
+  console.log(navItems);
 
   pageTitle.classList.toggle("page-title-active")
   menuBubbleTop.classList.toggle("menu-active-top")
   menuBubbleLower.classList.toggle("menu-active-lower")
-  
-
-  if (menuBubbleLower.classList.contains("menu-first-load")) {
-    menuBubbleLower.classList.remove("menu-first-load")
-  }
 
   if (menuOpen === true) {
     menuOpen = false;
@@ -28,11 +23,9 @@ const menuBtnHandler = () => {
   } else {
     menuOpen = true;
     animation.getLottie().playSegments([0, 14], true);
+  }
 
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].classList.toggle("nav-item-active");
   }
 };
-
-const searchBtnHandler = () => {
-  const animation = document.querySelector(".search-btn-icon");
-  animation.getLottie().goToAndPlay(0, true);
-}
