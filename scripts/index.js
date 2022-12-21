57db29383594f67c62478cb71f9ac10a906df451
Robin <robin.django@gmail.com>
2022-12-21 10:30:59 +0100

    bingbong

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,46 @@
-// Boolean variable to check if menu is open.
-// Assigned globally so it doesn't reassign on every click.
-var menuOpen = false;
+let menuActive = false;
+let searchActive = false;
 
 const menuBtnHandler = () => {
-  // Get all the elements we need to toggle/control
-  const animation = document.getElementById("menu-btn-icon");
-  const menuBubbleTop = document.getElementById("menu-bubble-top");
-  const menuBubbleLower = document.getElementById("menu-bubble-lower");
-  const pageTitle = document.getElementById("page-title");
-  const navItems = Array.from(document.getElementsByClassName("nav-item"));
+  // Get the current state of the header menus
+  let status = getHeaderStatus();
 
+  // Get all the elements we need to toggle/control
+  const elements = {
+    icon: document.getElementById("menu-btn-icon"),
+    topBubble: document.getElementById("menu-bubble-top"),
+    lowerBubble: document.getElementById("menu-bubble-lower"),
+    pageTitle: document.getElementById("page-title"),
+  }
+  // Did not work while in the object, so declaring it seperately
+  const navItems = Array.from(document.getElementsByClassName("nav-item"));
+  
   // Toggle all the active classes to start the animations
-  pageTitle.classList.toggle("page-title-active")
-  menuBubbleTop.classList.toggle("menu-active-top")
-  menuBubbleLower.classList.toggle("menu-active-lower")
+  elements.pageTitle.classList.toggle("page-title-active");
+  elements.topBubble.classList.toggle("menu-active-top");
+  elements.lowerBubble.classList.toggle("menu-active-lower");
 
   // Check if menu is opening or closing, and play the correct Lottie animation
-  if (menuOpen === true) {
-    menuOpen = false;
-    animation.getLottie().playSegments([14, 0], true);
+  if (status.menu) {
+    menuActive = false;
+    elements.icon.getLottie().playSegments([14, 0], true);
   } else {
-    menuOpen = true;
-    animation.getLottie().playSegments([0, 14], true);
-  }
+    menuActive = true;
+    elements.icon.getLottie().playSegments([0, 14], true);
+  };
 
   // Toggle the active class on the nav items
   for (let i = 0; i < navItems.length; i++) {
     navItems[i].classList.toggle("nav-item-active");
-  }
+  };
+};
+
+// Function to get the current state of the header menus
+const getHeaderStatus = () => {
+  return (
+    {
+      menu: menuActive,
+      search: searchActive,
+    }
+  );
 };
